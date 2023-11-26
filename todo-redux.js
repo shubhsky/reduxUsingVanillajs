345c5d4e414f77c041e7afd1af60200eafd74bee
Shubham Kumar <shubhamkr15060@gmail.com>
2023-11-26 12:40:45 +0530

    redux implemented

--- todo-redux.js
+++ todo-redux.js
@@ -18,17 +18,49 @@ const toggleToDo = (index) => ({
 
 // Initital State
 const initialState = {
-    todo:[]
+    todos:[]
 }
 
-// Reducer
+// Reducer - must be pure function
+// must return updated state if they are updating the state
 function todoReducer(state=initialState, action){
     switch(action.type){
         case ADD_TODO:
+            return {
+                ...state,
+                todos:[
+                    ...state.todos,
+                    {
+                        text:action.text,
+                        completed:false
+                    }
+                ]
+            }
         
         case TOOGLE_TODO:
+            return {
+                ...state,
+                todos:state.todos.map((todo,i)=>{
+                    if(i === action.index){
+                        todo.completed = !todo.completed
+                    }
+                    return todo
+                })
+            }
 
         default:
             return state
     }
 }
+
+// Creating store
+const store = redux.createStore(todoReducer);
+
+// dispatch actions..
+store.dispatch(addToDo('Study at 8'));
+store.dispatch(addToDo('Office at 9'));
+store.dispatch(toggleToDo(0));
+store.dispatch(addToDo('gym at 6'));
+
+// read data from store - selectors
+console.log(store.getState());
